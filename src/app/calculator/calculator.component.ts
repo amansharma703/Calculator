f8db2612d7f1aa8b1636fb2d3a3924abfa0184d0
Aman Sharma <72183900+amansharma703@users.noreply.github.com>
2023-03-02 11:49:12 +0530

    Change multiply symbol

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.', '-', '+', '*', '/', '=', 'CLEAR'];
+  buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.', '-', '+', 'x', '/', '=', 'CLEAR'];
   val = '';
 
   constructor() { }
